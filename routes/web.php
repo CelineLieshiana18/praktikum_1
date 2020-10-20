d7f139876736a918e980a71bb0e570fcba4a75e5
realebin <febrinafeb10@gmail.com>
2020-10-20 17:46:58 +0700

    api

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::post('/store', 'App\Http\Controllers\UserController@store')->name('store'
 Route::get('/destroy/{id}', 'App\Http\Controllers\UserController@destroy')->name('destroy');
 Route::get('/update/{id}', 'App\Http\Controllers\UserController@update')->name('update');
 Route::post('/save', 'App\Http\Controllers\UserController@save')->name('save');
+
+
+Route::get('/insertUser','App\Http\Controllers\UserController@insertUser')->name('insertUser');
+Route::get('/updateUser/{id}','App\Http\Controllers\UserController@updateUser')->name('updateUser');
+Route::get('/deleteUser/{id}','App\Http\Controllers\UserController@deleteUser')->name('deleteUser');
+Route::get('/getAllUser','App\Http\Controllers\UserController@getAllUser')->name('getAllUser');
+Route::get('/getOneUser/{id}','App\Http\Controllers\UserController@getOneUser')->name('getOneUser');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
