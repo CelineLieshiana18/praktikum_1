095fb5349e564621f26e97d7d9e4e5f0d45916f1
realebin <febrinafeb10@gmail.com>
2020-10-20 14:21:09 +0700

    fungsi api (blm routes)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -54,4 +54,53 @@ class UserController extends Controller
         $user->update();
         return redirect()->route('index');
     }
+
+
+
+    /////////////////////API///////////////////////
+
+    function getAllUser(Request $request){
+        $user = User::all();
+        return json_encode($user);
+    }
+
+    function getOneUser(Request $request,$id){
+        $user = User::where('id','=',$id)->get();
+        return json_encode($user);
+    }
+    
+    function deleteUser(Request $request,$id){
+        $data = User::findOrFail($id);
+        $data->delete();
+        $result = array();
+        $result["id"] = 1;
+        $result["message"] = 'Delete Successfull';
+        return json_encode($result);
+    }
+
+    function updateUser(Request $request,$id){
+        $user = User::findOrFail($id);
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->email = $request->email;
+        $user->gender = $request->gender;
+        $user->update();
+        $result["id"] = 1;
+        $result["message"] = 'Update Successfull';
+        return json_encode($result);
+    }
+
+    function insertUser(Request $request){
+        $user = new User();
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->email = $request->email;
+        $user->gender = $request->gender;
+        $user->save();
+        $result["id"] = 1;
+        $result["message"] = 'Insert Successfull';
+        return json_encode($result);
+    }
+
+    ///////////////////End of API/////////////////////
 }
